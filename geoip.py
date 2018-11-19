e695e96f17a10cd17bbd52715f0268f64aa9de87
d.gordeev <downtownxore@gmail.com>
2018-11-19 12:15:43 +0300

    working on saving results in file

--- geoip.py
+++ geoip.py
@@ -1,3 +1,9 @@
+def save(dict):
+    results = open('results', 'a', encoding='utf-8')
+
+    for key, value in dict.items():
+        print(f'{key}: {value}')
+
 def geo_lookup():
     import requests
     import xml.etree.ElementTree as ET
@@ -7,14 +13,17 @@ def geo_lookup():
     ip = input('> ')
     URL = f'http://api.geoiplookup.net/?query={ip}'
 
-    geo_response = requests.get(URL)
+    response = requests.get(URL)
 
-    if geo_response.status_code == 200:
-        root = ET.fromstring(geo_response.text)
+    if response.status_code == 200:
+        root = ET.fromstring(response.text)
 
         print('\n====================================')
+        geo_result = {}
         for child in root[0][0]:
-            print(f'{child.tag.title()}: {child.text}')
+            geo_result[child.tag.title()] = child.text
+            # print(f'{child.tag.title()}: {child.text}')
+        save(geo_result)
     else:
         print('Server returned not OK response, exiting.')
         exit(1)
