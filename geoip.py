44e76fe4ebb2ee5932555ea8a607b81f86552d26
d.gordeev <downtownxore@gmail.com>
2018-11-19 11:57:49 +0300

    initial commit

---
+++ geoip.py
@@ -0,0 +1,23 @@
+def geo_lookup():
+    import requests
+    import xml.etree.ElementTree as ET
+
+
+    print('Enter IP address:')
+    ip = input('> ')
+    # ip = '94.181.183.160'
+    URL = f'http://api.geoiplookup.net/?query={ip}'
+
+    geo_response = requests.get(URL)
+
+    if geo_response.status_code == 200:
+        root = ET.fromstring(geo_response.text)
+
+        print('\n====================================')
+        for child in root[0][0]:
+            print(f'{child.tag.title()}: {child.text}')
+    else:
+        print('Server returned not OK response, exiting.')
+        exit(1)
+
+geo_lookup()
