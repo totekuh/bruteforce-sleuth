6d22487cd80e39817b7ba3133fdbce5ee138fbe0
retr0 <you@example.com>
2018-11-28 18:09:32 -0500

    add geoip for the addresses list

---
+++ logparser.py
@@ -0,0 +1,16 @@
+import re
+
+
+def extract_ip_list(lines_to_skip=2):
+    ip_list = []
+    with open('log', 'r', encoding='utf-8') as f:
+        for log in list(f):
+            try:
+                ip = re.search(r'(?<=Received disconnect from ).\S*', log).group(0)
+                ip_list.append(ip)
+            except Exception as e:
+                None
+
+    return ip_list
+
+print(extract_ip_list())
