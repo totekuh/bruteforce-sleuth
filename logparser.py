e50f44eb2ef69f26089a26f775c43c77fe748896
mtalimanchuk <mtalimanchuk@gmail.com>
2018-11-29 13:55:13 +0300

    RegEx update. Now you can edit the ip_rgx variable to change the IP finder pattern

--- logparser.py
+++ logparser.py
@@ -1,16 +1,20 @@
 import re
 
 
+ip_rgx = r'(?<=Received disconnect from ).\S*'
+
+
 def extract_ip_list():
     ip_list = []
     with open('log', 'r', encoding='utf-8') as f:
         for log in list(f):
             try:
-                ip = re.search(r'(?<=Received disconnect from ).\S*', log).group(0)
+                ip = re.search(ip_rgx, log).group(0)
                 ip_list.append(ip)
             except Exception as e:
                 None
 
     return set(ip_list)
+
 
 print(extract_ip_list())
--- map.py
+++ map.py
@@ -1,8 +1,12 @@
 import geoip, logparser
 import folium
+import logging
 
+
+logging.basicConfig(level=logging.INFO)
 # Make an empty map
 m = folium.Map(location=[20, 0], tiles="Mapbox Bright", zoom_start=2)
+
 
 ip_list = logparser.extract_ip_list()
 for ip in ip_list:
