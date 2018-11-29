5c627ee5c1478c3152f3bbdfde10235fa955584d
Daniel <30957352+derstolz@users.noreply.github.com>
2018-11-29 12:09:02 +0300

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import geoip
-import logparser
-
-addresses = logparser.extract_ip_list(open('log', 'r').read())
-
-for ip in addresses:
-	print(f'Performing IP lookup for the IP address: {ip}')
-	geoip.geo_lookup(ip=ip.replace('\n', ''))
