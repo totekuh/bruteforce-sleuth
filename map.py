754786c368e95b8d195af1de0721f657b9a76ffb
mtalimanchuk <mtalimanchuk@gmail.com>
2018-12-02 20:08:31 +0300

    Final map version. Minor fixes and cleanup

--- map.py
+++ map.py
@@ -1,23 +1,21 @@
-import log_parser
+import csv
 import folium
 from folium.plugins import MarkerCluster
+import log_parser
 
 
 def create_map():
-    disconnections = log_parser.parse()
-
-    try:
-        world = folium.Map(location=[20, 0], tiles="Mapbox Bright", zoom_start=2)
-        mc = MarkerCluster()
+    world = folium.Map(location=[20, 0], tiles="Mapbox Bright", zoom_start=2)
+    mc = MarkerCluster()
+    with open(log_parser.csv_path, newline='') as csvfile:
+        disconnections = csv.DictReader(csvfile)
         for d in disconnections:
-            p = folium.Popup(f"{d.geo['Isp']} {d.geo['Ip']} {d.geo['City']}, {d.geo['Countrycode']}")
-            folium.Marker((float(d.geo['Latitude']), float(d.geo['Longitude'])),
+            p = folium.Popup(f"{d['Isp']} {d['Ip']} {d['City']}, {d['Countrycode']}")
+            folium.Marker((float(d['Latitude']), float(d['Longitude'])),
                           popup=p).add_to(mc)
-        world.add_child(mc)
-        world.save('map.html')
-        print('map.html successfully created')
-    except Exception as e:
-        print(f'Error creating map: {e}')
+    world.add_child(mc)
+    world.save('map.html')
+    print('map.html successfully created')
 
 
 create_map()
