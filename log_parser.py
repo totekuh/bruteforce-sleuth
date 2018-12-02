02e3a0ebfc6d8744e831a65b78823a7faeb19fa5
mtalimanchuk <mtalimanchuk@gmail.com>
2018-12-03 00:20:15 +0300

    Added ip list

--- log_parser.py
+++ log_parser.py
@@ -18,17 +18,21 @@ class DisconnectionLog:
 
 def parse():
     disconnections = []
+
     with open(log_path, 'r', encoding='utf-8') as f:
         for log in list(f):
             try:
                 m = re.search(rgx, log)
                 ip = m.group('ip')
                 response = m.group('response')
-                dlog = DisconnectionLog(log, ip, response)
-                if dlog.geo is not None:
-                    disconnections.append(dlog)
+                d = DisconnectionLog(log, ip, response)
+                if d.geo is not None:
+                    disconnections.append(d)
             except AttributeError:
                 pass
+
+    with open('list', 'w', encoding='utf-8') as ip_list_file:
+        ip_list_file.writelines('\n'.join([d.ip for d in disconnections]))
 
     with open(csv_path, 'w', newline='') as csv_file:
         fieldnames = ['Ip', 'Host', 'Isp', 'City', 'Countrycode', 'Countryname', 'Latitude', 'Longitude']
@@ -36,7 +40,7 @@ def parse():
         writer.writeheader()
         writer.writerows([d.geo for d in disconnections])
         print(f'Saved {len(disconnections)} log events as {csv_path}')
-    return disconnections
 
+    return disconnections
 
 parse()
