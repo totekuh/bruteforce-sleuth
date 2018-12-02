e0a77666463858733350366b317637bb31ed8f99
retr0 <retr0@home>
2018-12-02 23:18:28 +0100

    collecting shodan data into dict

--- shodan_search.py
+++ shodan_search.py
@@ -5,9 +5,6 @@ def get_apikey():
     print('Found stored api-key.')
     return api_key
 
-apikey = get_apikey()
-api = shodan.Shodan(apikey)
-
 def search_hosts(hostlist=None):
     if hostlist is None:
         print('No host list was provided. Using the ./hostlist as a default one.')
@@ -15,14 +12,19 @@ def search_hosts(hostlist=None):
     shodan_lookup = {}
     for host in hostlist:
         shodan_response = search(host)
-        print(shodan_response)
+        shodan_lookup[host] = shodan_response
+
+    return shodan_lookup
 
 def search(ip):
     host = api.host(ip)
-
     print('\n===========================================\n')
-
     for key, value in host.items():
         print(f'{key}: {value}')
-
     print('\n===========================================\n')
+    return host
+
+
+apikey = get_apikey()
+api = shodan.Shodan(apikey)
+search_hosts()
