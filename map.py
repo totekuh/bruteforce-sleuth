f067cdd4240e3d3ef764194a2be24e1b55681ba3
mtalimanchuk <mtalimanchuk@gmail.com>
2018-12-02 20:35:59 +0300

    Merge branch 'master' of C:\Projects\geo-ip-lookup with conflicts.

--- map.py
+++ map.py
@@ -1,21 +1,26 @@
 import csv
 import folium
 from folium.plugins import MarkerCluster
-import log_parser
 
 
-def create_map():
-    world = folium.Map(location=[20, 0], tiles="Mapbox Bright", zoom_start=2)
-    mc = MarkerCluster()
-    with open(log_parser.csv_path, newline='') as csvfile:
-        disconnections = csv.DictReader(csvfile)
+def create_map(mtype='default'):
+
+    disconnections = []
+    with open('disconnections.csv', 'r', newline='') as csv_file:
+        disconnections = csv.DictReader()
+
+    try:
+        world = folium.Map(location=[20, 0], tiles="Mapbox Bright", zoom_start=2)
+        mc = MarkerCluster()
         for d in disconnections:
-            p = folium.Popup(f"{d['Isp']} {d['Ip']} {d['City']}, {d['Countrycode']}")
-            folium.Marker((float(d['Latitude']), float(d['Longitude'])),
+            p = folium.Popup(f"{d.geo['Isp']} {d.geo['Ip']} {d.geo['City']}, {d.geo['Countrycode']}")
+            folium.Marker((float(d.geo['Latitude']), float(d.geo['Longitude'])),
                           popup=p).add_to(mc)
-    world.add_child(mc)
-    world.save('map.html')
-    print('map.html successfully created')
+        world.add_child(mc)
+        world.save('map.html')
+        print('map.html successfully created')
+    except Exception as e:
+        print(f'Error creating map: {e}')
 
 
 create_map()
